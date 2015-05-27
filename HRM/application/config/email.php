c247c304cefd98d63e4d4a36a3fb817a885d2f3c
indra <indra@INTEGRAL-IND.integral.local>
2015-05-27 11:39:17 +0700

    Add Mandrill via SMTP

--- HRM/application/config/email.php
+++ HRM/application/config/email.php
@@ -6,3 +6,13 @@
  * and open the template in the editor.
  */
 
+            $config['protocol'] = 'smtp';
+            $config['smtp_host'] = 'smtp.mandrillapp.com';
+            $config['smtp_user'] = 'smtp.mandrillapp.com';
+            $config['smtp_pass'] = 'smtp.mandrillapp.com';
+            $config['smtp_port'] = 'smtp.mandrillapp.com';
+            $config['smtp_timeout'] = 'smtp.mandrillapp.com';
+            $config['smtp_keepalive'] = 'smtp.mandrillapp.com';
+            $config['smtp_crypto'] = 'smtp.mandrillapp.com';
+            $config['charset'] = 'iso-8859-1';
+            $config['wordwrap'] = TRUE;
